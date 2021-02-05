c1edc5e0798c34111454d0323d5b4d122522e09e
H-Cong <nych1989@gmail.com>
2021-02-05 00:19:33 -0500

    20210205

---
+++ 258_AddDigits/258_AddDigits.py
@@ -0,0 +1,15 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        temp = 0
+        while num > 0:
+            temp += num % 10
+            num //= 10
+            if num == 0 and temp > 9:
+                num = temp
+                temp = 0
+        
+        return temp
+    
+    # TC: O(len(num))
+    
+    # SC: O(1)
